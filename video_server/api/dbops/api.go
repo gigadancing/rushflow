cea94512c407c98caa378565392a9c8a55eed2cf
ssuchichan <ssuchichan@163.com>
2018-12-28 23:37:21 +0800

    实现video

--- video_server/api/dbops/api.go
+++ video_server/api/dbops/api.go
@@ -3,6 +3,9 @@ package dbops
 import (
 	"database/sql"
 	"log"
+	"runshflow/video_server/api/defs"
+	"runshflow/video_server/api/utils"
+	"time"
 )
 
 // 增加用户
@@ -43,6 +46,66 @@ func DeleteUser(loginName string, pwd string) error {
 		return err
 	}
 	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
+	return nil
+}
+
+// 新增视频
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	t := time.Now()
+	cTime := t.Format("Jan 02 2006,15:04:05")
+	stmtIns, err := dbConn.Prepare("INSERT INTO video_info (id,author_id,name,display_ctime) VALUES(?,?,?,?)")
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmtIns.Exec(vid, aid, name, cTime)
+	if err != nil {
+		return nil, err
+	}
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: cTime}
+	defer stmtIns.Close()
+	return res, nil
+}
+
+// 查询视频
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+
+	return res, nil
+}
+
+// 删除视频
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id=?")
+	if err != nil {
+		return err
+	}
+	_, err = stmtDel.Exec(vid)
 	if err != nil {
 		return err
 	}
--- video_server/api/defs/apidef.go
+++ video_server/api/defs/apidef.go
@@ -5,3 +5,11 @@ type UserCredential struct {
 	Username string `json:"user_name"`
 	Password string `json:"password"`
 }
+
+// Data model
+type VideoInfo struct {
+	Id           string
+	AuthorId     int
+	Name         string
+	DisplayCtime string
+}
