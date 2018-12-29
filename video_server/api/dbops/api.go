f5355f4d52d835417d5455e77b7c701a976cb33c
ssuchichan <ssuchichan@163.com>
2018-12-29 13:52:36 +0800

    实现comment

--- video_server/api/dbops/api.go
+++ video_server/api/dbops/api.go
@@ -106,3 +106,44 @@ func DeleteVideoInfo(vid string) error {
 	defer stmtDel.Close()
 	return nil
 }
+
+// 新增评论
+func AddNewComments(vid string, aid int, content string) error {
+	id, err := utils.NewUUID()
+	if err != nil {
+		return err
+	}
+	stmtIns, err := dbConn.Prepare("INSERT INTO comments (id,video_id,author_id,content) VALUES (?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmtIns.Exec(id, vid, aid, content)
+	if err != nil {
+		return err
+	}
+	defer stmtIns.Close()
+	return nil
+}
+
+// 列出视频下所有评论
+func ListComments(vid string, from, to int) ([]*defs.Comment, error) {
+	// uers join comments -> author_id , video_id -> comment
+	stmtOut, err := dbConn.Prepare(`SELECT comments.id, users.login_name, comments.content FROM comments
+		INNER JOIN users ON comments.author_id = users.id
+		WHERE comments.video_id=? AND comments.time > FROM_UNIXTIME(?) AND comments.time <= FROM_UNIXTIME(?)`)
+	var res []*defs.Comment
+	rows, err := stmtOut.Query(vid, from, to)
+	if err != nil {
+		return res, err
+	}
+	for rows.Next() {
+		var id, authorName, content string
+		if err := rows.Scan(&id, &authorName, &content); err != nil {
+			return res, err
+		}
+		c := &defs.Comment{Id: id, VideoId: vid, Author: authorName, Content: content}
+		res = append(res, c)
+	}
+	defer stmtOut.Close()
+	return res, nil
+}
