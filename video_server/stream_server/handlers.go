be7b86e9971a9c2dcf4a973b337bed8e3f8484f8
ssuchichan <ssuchichan@163.com>
2019-01-02 11:25:04 +0800

    实现streaming handler

--- video_server/stream_server/handlers.go
+++ video_server/stream_server/handlers.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"os"
+	"time"
 )
 
 //
 func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-
+	vid := p.ByName("vid-id")
+	vl := VIDEO_DIR + vid
+	video, err := os.Open(vl)
+	if err != nil {
+		fmt.Println(err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+	w.Header().Set("Content-Type", "video/mp4")
+	http.ServeContent(w, r, "", time.Now(), video)
+	defer video.Close()
 }
 
 //
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 }
-
-
