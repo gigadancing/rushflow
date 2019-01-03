451695b3275018dcfb7e50eef00c4e9a180699e5
ssuchichan <ssuchichan@163.com>
2019-01-03 14:06:46 +0800

    实现runner的生产者消费者模型

--- video_server/scheduler/taskrunner/defs.go
+++ video_server/scheduler/taskrunner/defs.go
@@ -1,1 +1,17 @@
 package taskrunner
+
+// message
+const (
+	READY_TO_DISPATCH = "d"
+	READY_TO_EXECUTE  = "e"
+	CLOSE             = "c"
+)
+
+//
+type controlChan chan string
+
+//
+type dataChan chan interface{}
+
+//
+type fn func(dc dataChan) error
