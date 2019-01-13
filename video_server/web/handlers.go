b1f9b54cd2d7eb2c148df35d24199045f1b39b94
ssuchichan <ssuchichan@163.com>
2019-01-13 15:35:46 +0800

    实现homehandler和前端渲染

--- video_server/web/handlers.go
+++ video_server/web/handlers.go
@@ -23,14 +23,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		t   *template.Template
 		err error
 	)
-	p := &HomePage{
-		Name: "xiaodianying",
-	}
-	if t, err = template.ParseFiles("./templates/home.html"); err != nil {
-		log.Printf("Parsing templates home.html error:%v", err)
+
+	// visitor
+	// 取cookie
+	cname, err1 := r.Cookie("username")
+	sid, err2 := r.Cookie("session")
+
+	// cookie中没有登录信息
+	if err1 != nil || err2 != nil {
+		p := &HomePage{
+			Name: "chendada",
+		}
+		if t, err = template.ParseFiles("./templates/home.html"); err != nil {
+			log.Printf("Parsing templates home.html error:%v", err)
+			return
+		}
+		t.Execute(w, p)
 		return
 	}
-	//
-	t.Execute(w, p)
-	return
+
+	// user
+	// cookie中有登录信息
+	if len(cname.Value) != 0 && len(sid.Value) != 0 {
+		http.Redirect(w, r, "/userhome", http.StatusFound)
+		return
+	}
+
 }
